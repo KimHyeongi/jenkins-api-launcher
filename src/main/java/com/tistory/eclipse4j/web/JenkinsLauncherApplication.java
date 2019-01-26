1eea17c8c1f8ec6f4261f40f7a9fd049c4e51515
Grissom <eclipse4j.grissom@gmail.com>
2019-01-26 16:56:48 +0900

    Init Project

---
+++ src/main/java/com/tistory/eclipse4j/web/JenkinsLauncherApplication.java
@@ -0,0 +1,15 @@
+package com.tistory.eclipse4j.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * http://eclipse4j.tistory.com
+ */
+@SpringBootApplication
+public class JenkinsLauncherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsLauncherApplication.class, args);
+	}
+}
